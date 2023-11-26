5962fa5dbbfd8e28c3b772bb7ec4904756fb641f
Sasha <skalitsenia@mail.ru>
2023-11-26 23:03:12 +0300

    Creation and registration of service, DI, access modifiers

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ServiseService} from "../../servise/servise.service";
+
 interface Fruit {
   id: string
   name: string
   price: number
 }
+
 @Component({
   selector: 'app-parent',
   standalone: true,
@@ -13,17 +16,23 @@ interface Fruit {
   styleUrl: './parent.component.scss'
 })
 export class ParentComponent {
-text = "Hello my dream"
+  count = 0
+constructor(private ServiseService: ServiseService) {
+    setInterval(()=> {
+      this.count++
+    }, 1000)
+}
+  text = "Hello my dream"
 
   fruits: Fruit[] = [
-    { id: '1', name: 'apple', price: 10 },
-    { id: '2', name: 'orange', price: 20 },
-    { id: '3', name: 'watermelon', price: 30 },
-    { id: '4', name: 'banana', price: 5 },
-    { id: '5', name: 'pears', price: 12 },
-    { id: '6', name: 'raspberries', price: 18 },
-    { id: '7', name: 'avocados', price: 14 },
-    { id: '8', name: 'mangoes', price: 3 },
-    { id: '9', name: 'kiwifruit', price: 7 },
+    {id: '1', name: 'apple', price: 10},
+    {id: '2', name: 'orange', price: 20},
+    {id: '3', name: 'watermelon', price: 30},
+    {id: '4', name: 'banana', price: 5},
+    {id: '5', name: 'pears', price: 12},
+    {id: '6', name: 'raspberries', price: 18},
+    {id: '7', name: 'avocados', price: 14},
+    {id: '8', name: 'mangoes', price: 3},
+    {id: '9', name: 'kiwifruit', price: 7},
   ]
 }
---
+++ src/servise/servise.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiseService {
+  value = 0
+  constructor() { }
+
+}
