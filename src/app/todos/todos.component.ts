61c44f60b72233750a5f51c6deab3fefc0171498
Sasha <skalitsenia@mail.ru>
2023-11-27 19:17:57 +0300

    12.2 Post and delete requests

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -21,6 +21,8 @@ export class TodosComponent implements OnInit {
   }
 
   todo: Todo[] = []
+  title: string = ''
+  todoId = ''
 
   ngOnInit(): void {
     this.getTodos()
@@ -34,6 +36,38 @@ export class TodosComponent implements OnInit {
       }
     }).subscribe(res => {
       this.todo = res
+    })
+  }
+
+  onChangeTitle(event: Event) {
+    this.title = (event.currentTarget as HTMLInputElement).value
+    console.log((event.currentTarget as HTMLInputElement).value)
+  }
+
+  onChangeId(event: Event) {
+    this.todoId = (event.currentTarget as HTMLInputElement).value
+  }
+
+  creteTodo() {
+    this.http.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title: this.title}, {
+      withCredentials: true,
+      headers: {
+        "api-key": "02801dc9-c643-40b7-9ded-224fb486763d"
+      }
+    }).subscribe(res => {
+
+    })
+  }
+
+  deleteTodo() {
+    this.http.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${this.todoId}`, {
+      withCredentials: true,
+      headers: {
+        "api-key": "02801dc9-c643-40b7-9ded-224fb486763d"
+      }
+    }).subscribe(res => {
+      this.todo = this.todo.filter(t => t.id !== this.todoId)
+      console.log(res)
     })
   }
 }
